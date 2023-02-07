5ef217f3c69fb5fc49b1547a8477172c8b3d60e6
Enoch Kim <jihok0519@gmail.com>
2023-02-07 17:14:23 -0500

    writing logic for google pay

--- js/app.js
+++ js/app.js
@@ -23,3 +23,49 @@ function initMap() {
     // })
     
 }
+
+const tokenizationSPecification = {
+    type: 'PAYMENT_GATEWAY',
+    parameters: {
+        gateway: '',
+        gatewayMerchantId: '',
+    }
+}
+
+const cardPaymentMethod = {
+    type: 'CARD',
+    tokenizationSpecification: tokenizationSpecification,
+    parameters: {
+        allowedCardNetworks: ['VISA','MASTERCARD'],
+        allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+    }
+};
+
+const googlePayConfiguration = {
+    apiVersion: 2,
+    apiVersionMinor: 0,
+    allowedPaymentMethods: [cardPaymentMethod],
+};
+
+let googlePayClient;
+
+
+function onGooglePayLoaded() {
+    googlePayClient = new google.payments.api.PaymentsClient({
+        environment: 'TEST',
+    });
+
+    googlePayClient.isReadyToPay(googlePayConfiguration)
+    .then(response => {
+        if (response.result) {
+            createAndAddButtion();
+        }
+    })
+    .catch(error => console.error('isReadyToPay error:', error))
+}
+
+function createAndAddButton() {
+    const googlePayButton = googlePayClient.createButton({
+        onClick: onGooglePayButtonClicked
+    })
+}
