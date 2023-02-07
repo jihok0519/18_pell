c86e3ef35c5b583e1dc82d75e7f8983238c7c495
Enoch Kim <jihok0519@gmail.com>
2023-02-06 20:10:57 -0500

    zoomed map in more

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 function initMap() {
     var options = {
         center: { lat: 40.7149, lng:-73.9980 },
-        zoom: 17
+        zoom: 18
     }
 
     map = new google.maps.Map(document.getElementById('map'), options)
@@ -10,4 +10,16 @@ function initMap() {
         position: {lat:40.7149, lng: -73.9980},
         map: map
     })
+
+
+    // Marker nametag
+
+    // const detailWindow = new google.maps.InfoWindow({
+    //     content: "18 Pell",
+    // });
+
+    // marker.addListener("mouseover", () => {
+    //     detailWindow.open(map, marker);
+    // })
+    
 }
