7b2f32bce5d76d3be8a9b34590a038a65b5e75d4
Enoch Kim <jihok0519@gmail.com>
2023-02-07 17:35:05 -0500

    payment process logic

--- js/app.js
+++ js/app.js
@@ -66,6 +66,37 @@ function onGooglePayLoaded() {
 
 function createAndAddButton() {
     const googlePayButton = googlePayClient.createButton({
-        onClick: onGooglePayButtonClicked
+        onClick: onGooglePayButtonClicked,
+    });
+
+    document.getElementById('buy-now').appendChild(googlePayButton);
+}
+
+function onGooglePayButtonClicked() {
+    const paymentDataRequest = { ...googlePayConfiguration };
+    paymentDataRequest.merchantInfo = {
+        merchantId: 'BCR2DN4TXLJJJILM',
+        merchantName: '18 Pell',
+    };
+
+    paymentDataRequest.transactionInfo = {
+        totalPriceStatus: 'FINAL',
+        totalPrice: selectedItem.price,
+        currencyCode: 'USD',
+        countryCode: 'US',
+    };
+
+    googlePayClient.loadPaymentData(paymentDataRequest)
+        .then(paymentData => processPaymentData(paymentData))
+        .catch(error => console.error('loadPaymentData error: ', error));
+}
+
+function processPaymentData(paymentData) {
+    fetch(ordersEndpointUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: paymentData
     })
 }
