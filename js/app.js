bb3dd15bb5a7244bd40599faf7c9a8c01eb3eb8c
Enoch Kim <jihok0519@gmail.com>
2023-02-15 20:43:12 -0500

    commit

--- js/app.js
+++ js/app.js
@@ -55,93 +55,83 @@ function initMap() {
         position: {lat:40.7149, lng: -73.9980},
         map: map
     })
-
-
-    // Marker nametag
-
-    // const detailWindow = new google.maps.InfoWindow({
-    //     content: "18 Pell",
-    // });
-
-    // marker.addListener("mouseover", () => {
-    //     detailWindow.open(map, marker);
-    // })
-    
 }
 
-const tokenizationSPecification = {
-    type: 'PAYMENT_GATEWAY',
-    parameters: {
-        gateway: '',
-        gatewayMerchantId: '',
-    }
-}
+// GOOGLE PAY INTEGRATION
 
-const cardPaymentMethod = {
-    type: 'CARD',
-    tokenizationSpecification: tokenizationSpecification,
-    parameters: {
-        allowedCardNetworks: ['VISA','MASTERCARD'],
-        allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
-    }
-};
+// const tokenizationSpecification = {
+//     type: 'PAYMENT_GATEWAY',
+//     parameters: {
+//         gateway: '',
+//         gatewayMerchantId: '',
+//     }
+// }
 
-const googlePayConfiguration = {
-    apiVersion: 2,
-    apiVersionMinor: 0,
-    allowedPaymentMethods: [cardPaymentMethod],
-};
+// const cardPaymentMethod = {
+//     type: 'CARD',
+//     tokenizationSpecification: tokenizationSpecification,
+//     parameters: {
+//         allowedCardNetworks: ['VISA','MASTERCARD'],
+//         allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+//     }
+// };
 
-let googlePayClient;
+// const googlePayConfiguration = {
+//     apiVersion: 2,
+//     apiVersionMinor: 0,
+//     allowedPaymentMethods: [cardPaymentMethod],
+// };
+
+// let googlePayClient;
 
 
-function onGooglePayLoaded() {
-    googlePayClient = new google.payments.api.PaymentsClient({
-        environment: 'TEST',
-    });
+// function onGooglePayLoaded() {
+//     googlePayClient = new google.payments.api.PaymentsClient({
+//         environment: 'TEST',
+//     });
 
-    googlePayClient.isReadyToPay(googlePayConfiguration)
-    .then(response => {
-        if (response.result) {
-            createAndAddButtion();
-        }
-    })
-    .catch(error => console.error('isReadyToPay error:', error))
-}
+//     googlePayClient.isReadyToPay(googlePayConfiguration)
+//     .then(response => {
+//         if (response.result) {
+//             createAndAddButtion();
+//         }
+//     })
+//     .catch(error => console.error('isReadyToPay error:', error))
+// }
 
-function createAndAddButton() {
-    const googlePayButton = googlePayClient.createButton({
-        onClick: onGooglePayButtonClicked,
-    });
+// function createAndAddButton() {
+//     const googlePayButton = googlePayClient.createButton({
+//         onClick: onGooglePayButtonClicked,
+//     });
 
-    document.getElementById('buy-now').appendChild(googlePayButton);
-}
+//     document.getElementById('buy-now').appendChild(googlePayButton);
+// }
 
-function onGooglePayButtonClicked() {
-    const paymentDataRequest = { ...googlePayConfiguration };
-    paymentDataRequest.merchantInfo = {
-        merchantId: 'BCR2DN4TXLJJJILM',
-        merchantName: '18 Pell',
-    };
+// function onGooglePayButtonClicked() {
+//     const paymentDataRequest = { ...googlePayConfiguration };
+//     paymentDataRequest.merchantInfo = {
+//         merchantId: 'BCR2DN4TXLJJJILM',
+//         merchantName: '18 Pell',
+//     };
 
-    paymentDataRequest.transactionInfo = {
-        totalPriceStatus: 'FINAL',
-        totalPrice: selectedItem.price,
-        currencyCode: 'USD',
-        countryCode: 'US',
-    };
+//     paymentDataRequest.transactionInfo = {
+//         totalPriceStatus: 'FINAL',
+//         totalPrice: selectedItem.price,
+//         currencyCode: 'USD',
+//         countryCode: 'US',
+//     };
 
-    googlePayClient.loadPaymentData(paymentDataRequest)
-        .then(paymentData => processPaymentData(paymentData))
-        .catch(error => console.error('loadPaymentData error: ', error));
-}
+//     googlePayClient.loadPaymentData(paymentDataRequest)
+//         .then(paymentData => processPaymentData(paymentData))
+//         .catch(error => console.error('loadPaymentData error: ', error));
+// }
 
-function processPaymentData(paymentData) {
-    fetch(ordersEndpointUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: paymentData
-    })
-}
+// function processPaymentData(paymentData) {
+//     fetch(ordersEndpointUrl, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: paymentData
+//     })
+// }
